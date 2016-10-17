572a6783d11d84c41000b01331842de190f1c8b9
mkobar <mkobar@rkosecurity.com>
2016-10-17 16:06:40 -0400

    added assets

---
+++ Assets/Martin/Scripts/CloudMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMover : MonoBehaviour {
+
+	public Vector3 direction = new Vector3(0f, 0f, 1f);
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(direction * speed * Time.deltaTime,  Space.Self);
+		if (transform.localPosition.z > 2200f) {
+			transform.Translate(new Vector3(0f,0f,-2200f));
+		}
+	}
+}
